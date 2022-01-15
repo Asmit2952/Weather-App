42e2f5eef55065e33bba596ae2ad0bacdef9e689
Asmit2952 <asmitbm2952002@gmail.com>
2022-01-15 16:25:36 +0530

    Weather app

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ let weather = {
     fetchWeather: function (city) {
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=metric&appid=" + this.apiKey
-        ).then((response) => response.json())
+        ).then((response) => {
+            if (!response.ok) {
+              alert("No weather found.");
+              throw new Error("No weather found.");
+            }
+            return response.json();
+          })
         .then((data) => this.displayWeather(data));
     },
     displayWeather: function (data) {
@@ -18,6 +24,8 @@ let weather = {
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind Speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
     },
     search: function () {
         this.fetchWeather(document.querySelector(".search-bar").value);
