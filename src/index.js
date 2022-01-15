7c38bd6b317c1c02ce7d54aebf1021726ed1874d
Asmit2952 <asmitbm2952002@gmail.com>
2022-01-15 15:25:45 +0530

    Api key inserted

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 let weather = {
     "apiKey": "41b3fd066db6bc19b0d47cc9b8de170a",
-    fetchWeather: function () {
+    fetchWeather: function (city) {
         fetch(
-            ""
+            "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=metric&appid=" + this.apiKey
         ).then((response) => response.json())
         .then((data) => console.log(data));
     },
+    displayWeather: function (data) {
+        
+    }
 };
