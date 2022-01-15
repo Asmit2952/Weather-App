4fc69f45cb86e3b4ddedc3dae016171396304554
Asmit2952 <asmitbm2952002@gmail.com>
2022-01-15 17:14:37 +0530

    Added geolocation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 let weather = {
-    "apiKey": "41b3fd066db6bc19b0d47cc9b8de170a",
+    "apiKey": config.WEATHER_API,
     fetchWeather: function (city) {
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=metric&appid=" + this.apiKey
@@ -38,6 +38,59 @@ let weather = {
     }
 };
 
+let geocode = {
+    reverseGeocode: function (latitude, longitude) {
+        var api_key = config.GEOLOCATION_API;
+      
+        var api_url = 'https://api.opencagedata.com/geocode/v1/json'
+      
+        var request_url = api_url
+          + '?'
+          + 'key=' + api_key
+          + '&q=' + encodeURIComponent(latitude + ',' + longitude)
+          + '&pretty=1'
+          + '&no_annotations=1';
+      
+        var request = new XMLHttpRequest();
+        request.open('GET', request_url, true);
+      
+        request.onload = function() {
+         
+          if (request.status === 200){ 
+           
+            var data = JSON.parse(request.responseText);
+            weather.fetchWeather(data.results[0].components.county);
+      
+          } else if (request.status <= 500){ 
+                                 
+            console.log("unable to geocode! Response code: " + request.status);
+            var data = JSON.parse(request.responseText);
+            console.log('error msg: ' + data.status.message);
+          } else {
+            console.log("server error");
+          }
+        };
+      
+        request.onerror = function() {
+          console.log("unable to connect to server");        
+        };
+      
+        request.send();  
+    },
+    getLocation: function () {
+        function success (data) {
+            geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
+        }
+
+        if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, console.error);
+        }
+        else {
+            weather.fetchWeather("Pune");
+        }
+    }
+};
+
 document.querySelector(".search button").addEventListener("click", function () {
     weather.search();
 });
@@ -48,4 +101,4 @@ document.querySelector(".search-bar").addEventListener("keyup", function (event)
     }
 });
 
-weather.fetchWeather("Pune");
+geocode.getLocation();
